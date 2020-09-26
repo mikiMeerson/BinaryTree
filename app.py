e713967e7bdd0cfb34e62b3af9185784e2a1ed58
Miki Meerson <mikimeerson44@gmail.com>
2020-09-26 13:01:17 +0300

    fix nearest_righ func

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 import random
 
+COUNT = [10]
+
 
 class Point:
     def __init__(self, x=None, y=None):
         # TODO: if the x/y is zero then we will rand number
         # random.randint- Return a random integer N such that a <= N <= b.
-        self.x = x if x else random.randint(0, 100)
-        self.y = y if y else random.randint(0, 100)
+        self.x = x if x is not None else random.randint(1, 100)
+        self.y = y if y is not None else random.randint(1, 100)
 
     def __le__(self, other: int):
         """ This function overrides '<' operator for points
@@ -24,6 +26,9 @@ class Point:
             Returns True if this point is to the right of other point, else False """
         return self.x < other.x
 
+    def __str__(self):
+        return "({0},{1})".format(self.x, self.y)
+
 
 class Node:
     def __init__(self, key: Point):
@@ -32,8 +37,12 @@ class Node:
         self.left = None
         self.parent = None
 
+    def __str__(self):
+        return str(self.key)
+
 
 class Tree:
+
     def __init__(self, root=None):
         self.root = root
 
@@ -59,24 +68,73 @@ class Tree:
 def build_tree() -> Tree:
     """ Create 1000 random points and insert them into a binary search tree """
     t = Tree()
-    for i in range(1000):
+    for i in range(10):
         new_node = Node(Point())
         t.insert(new_node)
     return t
 
 
-def nearest_right_point(t: Tree, x0: int) -> Point:
-    p = t.root
-    while (p.key <= x0 and p.right):
-        p = p.right
+def print_2d_util(root, space):
+    if root is None:
+        return
 
-    if p.key > x0:
-        return(p.key)
-    else:  # not p.right:
-        return Point(1, 1)
+    # Increase distance between levels
+    space += COUNT[0]
+
+    # Process right child first
+    print_2d_util(root.right, space)
+
+    # Print current node after space
+    # count
+    print()
+    for i in range(COUNT[0], space):
+        print(end=" ")
+    print(str(root.key.x) + ', ' + str(root.key.y))
+
+    # Process left child
+    print_2d_util(root.left, space)
+
+
+def print_2d(root):
+    print_2d_util(root, 0)
+
+
+def nearest_right_point(root, target):
+    def _get_closest_node(node1, node2=None):
+        """ Util func to get closest out of two nodes"""
+        if node2 is None or node1.key < node2.key:
+            return node1
+        if node1.key < node2.key:
+            return node1
+        return node2
+
+    # Eliminate side of tree that is to the left of the target
+    while root.key <= target and root.right:
+        root = root.right
+
+    # If root is to the left of target and there is nothing to its right
+    if root.key <= target:
+        return None
+
+    # Get nearest for left and right sub-trees
+    right_closest = None
+    left_closest = None
+
+    if root.left:
+        left_closest = nearest_right_point(root.left, target)
+    left_closest = _get_closest_node(root, left_closest)
+
+    if root.right:
+        right_closest = nearest_right_point(root.right, target)
+    right_closest = _get_closest_node(root, right_closest)
+
+    return _get_closest_node(right_closest, left_closest)
 
 
 if __name__ == '__main__':
+    t = build_tree()
+    print_2d(t.root)
     x0 = random.randint(0, 100)
-    point = nearest_right_point(build_tree(), x0)
-    print("{0},{1}".format(point.x, point.y))
+    print(" Searching for {} nearest to the right".format(x0))
+    point = nearest_right_point(t.root, x0)
+    print(point) if point else print(Point(0, 0))
